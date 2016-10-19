5c986527c2333cfc1591b8ef44f163974eb0aaa4
Jessica Breckenridge <jessicabreckenridge@gmail.com>
2016-10-18 18:09:13 -0600

    - PHP git.

--- phpunit/indexTest.php
+++ phpunit/indexTest.php
@@ -8,7 +8,7 @@ class indexTest extends PHPUnit_Framework_TestCase
 		include('www/index.php');
 		$output = ob_get_clean();
 		ob_end_clean();
-		$this->assertEquals("Automation for the people");
+		$this->assertSame("Automation for the people", $helpscouttagline);
 		}
 	}
 
